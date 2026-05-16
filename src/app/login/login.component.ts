03783a3f424d8a3151c37c563596a756a54bd44d
mehreen2103 <mehreenansari2097@gmail.com>
2026-05-16 12:44:56 +0530

    done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { HttpServiceService } from '../http-service.service';
 
 @Component({
   selector: 'app-login',
@@ -7,15 +8,28 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(public httpService: HttpServiceService ){}
+
+  endpoint: any = "http://localhost:8081/Auth/login"
+
  form:any = {
-    data:{}
+    data:{},
+    inputerror:{},
+    message: '',
+    errorMessage:'',
   }
   
-  SignIn(){
-
-    console.log("loginId: " + this.form.data.loginId );
-    console.log("password: " + this.form.data.password );
-
+   signIn() {
+    let self = this;
+    this.httpService.post(this.endpoint, this.form.data, (response: any) => {
+      console.log('response === >', response);
+      if (response.success == false && response.result.inputerror) {
+        self.form.inputerror = response.result.inputerror;
+      } else if (response.success == false && response.result.message) {
+        self.form.errorMessage = response.result.message;
+      } else if (response.success == true) {
+        self.form.message = response.result.message;
+      }
+    });
   }
-
 }
